9c9fb90d91e65ea73de04ce5c30bb22f8aeae0de
Kevin Hyungdo Joo <hjoo1@student.gsu.edu>
2022-12-01 14:53:50 -0500

    coding register/login

---
+++ CIS3270_Project_2/src/edu/gsu/gui/Controller.java
@@ -0,0 +1,51 @@
+package edu.gsu.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller implements Initializable{
+	
+	
+	@FXML
+	private TextField tf_username;
+	@FXML
+	private TextField tf_password;
+	@FXML
+	private Button button_login;
+	@FXML
+	private Button button_signup;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		button_login.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				DBUtils.loginUser(event, tf_username.getText()t, tf_password.getText());
+				
+			}
+
+		});
+				
+		button_signup.setOnAction(new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent event) {
+				DBUtils.signUp(event, "Register.fxml", "Registration");
+				
+			}
+			
+		});
+				
+		
+		
+		
+	}
+
+}
