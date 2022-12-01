1c7acb9bb2c2c4ceecc1078b5b23086173b42c34
Kevin Hyungdo Joo <hjoo1@student.gsu.edu>
2022-11-30 23:14:48 -0500

    123123

--- CIS3270_Project_2/src/module-info.java
+++ CIS3270_Project_2/src/module-info.java
@@ -1,6 +1,8 @@
 module CIS3270_Project_1 {
 	requires javafx.controls;
 	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens edu.gsu.gui to javafx.graphics, javafx.fxml;
 }
