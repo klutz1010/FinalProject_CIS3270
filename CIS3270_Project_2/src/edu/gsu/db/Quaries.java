e61334c3dfb30a0d6229cb686a1ab7c1af503b5d
klutz <klutz@KJaySwift>
2022-12-05 17:52:00 -0500

    deleting unnecessary packages

--- CIS3270_Project_2/src/edu/gsu/db/Quaries.java
+++
@@ -1,7 +0,0 @@
-package edu.gsu.db; //copy and paste from professor's google drive need to work on this.
-
-public class Quaries {
-	
-	public static final String LOGIN = "SELECT count(*) as num  FROM login_credentials WHERE username = ? AND password =?";
-
-}
