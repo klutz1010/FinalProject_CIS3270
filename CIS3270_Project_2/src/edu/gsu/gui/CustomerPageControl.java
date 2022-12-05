1ffa460385426198290455e00434c8999d4872f4
harve <harve@10.0.0.38>
2022-12-05 17:25:24 -0500

    Added startSearch files

--- CIS3270_Project_2/src/edu/gsu/gui/CustomerPageControl.java
+++ CIS3270_Project_2/src/edu/gsu/gui/CustomerPageControl.java
@@ -2,7 +2,6 @@ package edu.gsu.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.*;
@@ -16,6 +15,9 @@ public class CustomerPageControl implements Initializable {
 	
 	@FXML
 	private Button button_logout;
+	
+	@FXML
+    private Button searchFlightButton;
 	
 
 	@Override
@@ -31,12 +33,24 @@ public class CustomerPageControl implements Initializable {
 			
 		});
 		
-	}
+	
+	// Use Search Flight button to go to next scene
+		searchFlightButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+	
+				Action.changeScene(event, "StartSearch.fxml", "Search Flight", null);
+				
+			}
+		
+	});
+}
 
 	public void setUserInformation(String userName) {
 			
 			label_hello.setText(("Welcome" + userName + " !"));
 				
 		}
+	
 	
 }
