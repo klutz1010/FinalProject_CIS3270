4768fe316666a3a689c4ac02b2b72c46c72418fe
klutz <klutz@KJaySwift>
2022-12-13 19:51:36 -0500

    fixing

--- CIS3270_Project_2/src/edu/gsu/gui/Action.java
+++ CIS3270_Project_2/src/edu/gsu/gui/Action.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import edu.gsu.common.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -14,8 +15,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-
+// ACTION!!!!!
 public class Action {
+	
+	Customer data = Customer.getStoredUserName();
+	
+	//DB Connection method
+    public static Connection getConnection() {
+    	
+    	Connection connection = null;
+    	
+    	try {connection = DriverManager.getConnection
+				      ("jdbc:sqlserver://cis3270finalproject.database.windows.net:1433;"
+								+ "database=Project;user=cis3270admin@cis3270finalproject;password={Cis3270finalproject};"
+								+ "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+    	}  catch (Exception ex) {
+    		System.out.println("Error: " + ex.getMessage());
+    		return null;
+    	}
+		return connection;
+    }
 	
 	public static void changeScene(ActionEvent event, String fxmlFile, String title, String userName) {
 		
@@ -28,7 +47,7 @@ public class Action {
 				FXMLLoader loader = new FXMLLoader(Action.class.getResource(fxmlFile));
 				root = loader.load();
 				CustomerPageControl CustomerPageControl =loader.getController();
-				CustomerPageControl.setUserInformation(userName);
+				//CustomerPageControl.setUserInformation(userName);
 				
 			} catch (IOException e) {
 				
@@ -166,7 +185,7 @@ public class Action {
 		
 	}
 	
-	public static void loginUser(ActionEvent event, String username, String password) {
+	public static void loginUser(ActionEvent event, String userName, String userPassword) {
 		
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
@@ -178,7 +197,7 @@ public class Action {
 					+ "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 			
 			preparedStatement = connection.prepareStatement("SELECT userPassword FROM CustomerData WHERE userName = ? ");
-			preparedStatement.setString(1, username);
+			preparedStatement.setString(1, userName);
 			resultSet = preparedStatement.executeQuery();
 			
 			if (!resultSet.isBeforeFirst()) {
@@ -195,9 +214,9 @@ public class Action {
 					//getting password form the resultset
 					String retrievedPassword = resultSet.getString("userPassword"); 
 					// if password is correct, change the scene.
-					if (retrievedPassword.equals(password)) {
+					if (retrievedPassword.equals(userPassword)) {
 						
-							changeScene(event, "CustomerPage.fxml", "Welcome", null);
+							changeScene(event, "CustomerPage.fxml", "Welcome", " " + userName);
 
 					// if password is incorrect show alert.
 					} else {
--- CIS3270_Project_2/src/edu/gsu/gui/CustomerPageControl.java
+++ CIS3270_Project_2/src/edu/gsu/gui/CustomerPageControl.java
@@ -2,7 +2,6 @@ package edu.gsu.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import edu.gsu.common.Customer;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -19,10 +18,10 @@ public class CustomerPageControl implements Initializable {
 	private Button button_logout;
 	
 	@FXML
-    private Button button_logout11;
+    private Button button_searchflight;
 	
 	@FXML
-    private Button button_logout1;
+    private Button button_yourflight;
 	
 	Customer data = Customer.getStoredUserName();
 
@@ -41,7 +40,7 @@ public class CustomerPageControl implements Initializable {
 		
 	
 	// Use Search Flight button to go to next scene (error)
-		button_logout11.setOnAction(new EventHandler<ActionEvent>() {
+		button_searchflight.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
 	
@@ -51,7 +50,7 @@ public class CustomerPageControl implements Initializable {
 		
 		});
 					
-		button_logout1.setOnAction(new EventHandler<ActionEvent>() {
+		button_yourflight.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
 					
@@ -65,5 +64,5 @@ public class CustomerPageControl implements Initializable {
 		label_hello.setText("Welcome" + data.getUserName() + " !");
 		
 	}
-
+	
 }
